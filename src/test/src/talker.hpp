bb9d3f9c9b07205fb15bdaa2575e7dcce93b5fbd
daifuku07 <fuku137.di@gmail.com>
2020-01-09 23:03:05 +0900

    initial commit

---
+++ src/test/src/talker.hpp
@@ -0,0 +1,26 @@
+/* Copyright 2019 Daisuke Fukutomi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ROS2_EXAMPLE_WS_TEST_TALKER_HPP_
+#define ROS2_EXAMPLE_WS_TEST_TALKER_HPP_
+
+class Talker
+{
+public:
+  int a = 100;
+  int b = 200;
+};
+
+#endif  // ROS2_EXAMPLE_WS_TEST_TALKER_HPP_
